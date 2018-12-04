c12598da3eeb9ac78bbfe53e33274d75f02a8d29
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2018-12-04 04:13:55 +0200

    end the thing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/chat', function(req, res, next) {
+  
+  res.io.on('connection',function(socket){
+  	console.log('user connected');
+  	socket.on('chat message', function(msg){
+    res.io.emit('chat message', msg);
+    });
+  	socket.on('disconnect', function(){
+    console.log('user disconnected');
+    });
+  });
+  res.render('chat', { title: 'chatmm' });
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,10 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	res.io.on('connection', function(socket){
-  console.log('a user connected');
+      console.log('a user connected');
+      socket.on('disconnect', function(){
+      console.log('user disconnected');
+      });
     });
 
   res.render('index',{title:"index"});
